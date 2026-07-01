ab3d8112b39f99e483dc576ef09aa57e97ff1a19
Andrew-Velox <mohabbat.bd2020@gmail.com>
2026-07-02 03:58:32 +0600

    feat: config update

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Optimized for Vercel deployment
-}
+  output: 'export',
+  // Next.js's default image optimization doesn't work on static hosts, 
+  // so you must disable it for the build to succeed.
+  images: {
+    unoptimized: true,
+  },
+};
 
-module.exports = nextConfig
+export default nextConfig;
